181073691c05e2fe7dc92db2f34018f96da280ed
Кривицкий Дмитрий <dmitriy_serga@mail.ru>
2023-10-14 00:32:38 +0300

    fixHandler

--- goApi.go
+++ goApi.go
@@ -3,7 +3,7 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-import "goApi/heandlers"
+import "goApi/handlers"
 
 
 
@@ -11,8 +11,8 @@ func main() {
 	r := gin.Default()
 	v1 := r.Group("/hello")
 	{
-		v1.GET("/any", heandlers.Any)
-		v1.GET("/vi", heandlers.Vi)
+		v1.GET("/any", handlers.Any)
+		v1.GET("/vi", handlers.Vi)
 	}
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
