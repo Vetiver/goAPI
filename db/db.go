258168356a2b9971201f908d67b379dc763f3b3d
Vetiver <dmitriy_serga@mail.ru>
2023-10-16 00:19:31 +0300

    add Insert any

--- db/db.go
+++ db/db.go
@@ -2,20 +2,13 @@ package db
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type Record struct {
-	Id    int    `json:"id"`
-	Name  string `json:"name"`
-	Phone string `json:"phone"`
-  }
+
 
 func DbStart() {
 	urlExample := "postgres://postgres:228@localHost:5432/postgres"
@@ -37,41 +30,3 @@ func DbStart() {
 
 	fmt.Println(greeting)
 }
-
-func Insert(p *pgxpool.Pool, w http.ResponseWriter, r *http.Request) {
-	var rec Record
-	err := json.NewDecoder(r.Body).Decode(&rec)
-	if err != nil { // bad request
-	  w.WriteHeader(400)
-	  return
-	}
-  
-	conn, err := p.Acquire(context.Background())
-	if err != nil {
-		fmt.Errorf("Unable to acquire a database connection: %v\n", err)
-	  w.WriteHeader(500)
-	  return
-	}
-	defer conn.Release()
-  
-	row := conn.QueryRow(context.Background(),
-	  "INSERT INTO phonebook (name, phone) VALUES ($1, $2) RETURNING id",
-	  rec.Name, rec.Phone)
-	var id uint64
-	err = row.Scan(&id)
-	if err != nil {
-		fmt.Errorf("Unable to INSERT: %v\n", err)
-	  w.WriteHeader(500)
-	  return
-	}
-  
-	resp := make(map[string]string, 1)
-	resp["id"] = strconv.FormatUint(id, 10)
-	w.Header().Set("Content-Type", "application/json; charset=utf-8")
-	err = json.NewEncoder(w).Encode(resp)
-	if err != nil {
-		fmt.Errorf("Unable to encode json: %v\n", err)
-	  w.WriteHeader(500)
-	  return
-	}
-  }
--- handlers/any.go
+++ handlers/any.go
@@ -1,9 +1,56 @@
 package handlers
 
 import "github.com/gin-gonic/gin"
-import "goApi/db"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"strconv"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+type Record struct {
+	Id    int    `json:"id"`
+	Name  string `json:"name"`
+	Phone string `json:"phone"`
+  }
 
 func Any(c *gin.Context) {
-	var result = db.Insert
-	c.JSON(200, result)
+	c.JSON(200, )
 }
+
+func Insert() string {
+	urlExample := "postgres://postgres:228@localHost:5432/postgres"
+	var rec Record
+	err := json.NewDecoder().Decode(&rec)
+	if err != nil { // bad request
+	  return ""
+	}
+  
+	conn, err := pgxpool.New(context.Background(), urlExample)
+	if err != nil {
+		fmt.Errorf("Unable to acquire a database connection: %v\n", err)
+	  return ""
+	}
+  
+	row := conn.QueryRow(context.Background(),
+	  "INSERT INTO phonebook (name, phone) VALUES ($1, $2) RETURNING id",
+	  rec.Name, rec.Phone)
+	var id uint64
+	err = row.Scan(&id)
+	if err != nil {
+		fmt.Errorf("Unable to INSERT: %v\n", err)
+
+	  return ""
+	}
+  
+	resp := make(map[string]string, 1)
+	resp["id"] = strconv.FormatUint(id, 10)
+	err = json.NewEncoder().Encode(resp)
+	if err != nil {
+		fmt.Errorf("Unable to encode json: %v\n", err)
+
+	  return ""
+	}
+	return "что-то"
+  }
