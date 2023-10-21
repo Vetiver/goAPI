174a45e01864905d0ca54cd429522486fa86f929
Кривицкий Дмитрий <dmitriy_serga@mail.ru>
2023-10-21 15:39:18 +0300

    insert

--- db/db.go
+++ db/db.go
@@ -4,14 +4,15 @@ import (
 	"context"
 	"fmt"
 	"os"
-
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type Record struct {
+	Id    int    `json:"id"`
+  }
 
-
-func DbStart() {
-	urlExample := "postgres://postgres:228@localHost:5432/postgres"
+func DbStart() *pgxpool.Pool {
+	urlExample := "postgres://postgres:228@localhost:5432/postgres"
 	dbpool, err := pgxpool.New(context.Background(), urlExample)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
@@ -19,14 +20,27 @@ func DbStart() {
 	}
 	defer dbpool.Close()
 
-	var greeting string
-	err = dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
-	
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
-	
-
-	fmt.Println(greeting)
+	return dbpool
 }
+
+func Insert() string {
+	pool := DbStart()
+  
+	conn, err := pool.Acquire(context.Background())
+	//Acqure - забирает одно соединение с бд из pool
+	if err != nil {
+		fmt.Errorf("Unable to acquire a database connection: %v\n", err)
+	  return ""
+	}
+  
+	row := conn.QueryRow(context.Background(),
+	  "INSERT INTO test(name) VALUES ($1);", "gay")
+	var id uint64
+	err = row.Scan(&id)
+	if err != nil {
+		fmt.Errorf("Unable to INSERT: %v\n", err)
+
+	  return ""
+	}
+	return "что-то"
+  }
--- handlers/any.go
+++ handlers/any.go
@@ -1,56 +1,13 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"strconv"
-
-	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/gin-gonic/gin"
+	"goApi/db"
 )
-type Record struct {
-	Id    int    `json:"id"`
-	Name  string `json:"name"`
-	Phone string `json:"phone"`
-  }
+
+
 
 func Any(c *gin.Context) {
-	c.JSON(200, )
+	c.JSON(200, db.Insert())
 }
 
-func Insert() string {
-	urlExample := "postgres://postgres:228@localHost:5432/postgres"
-	var rec Record
-	err := json.NewDecoder().Decode(&rec)
-	if err != nil { // bad request
-	  return ""
-	}
-  
-	conn, err := pgxpool.New(context.Background(), urlExample)
-	if err != nil {
-		fmt.Errorf("Unable to acquire a database connection: %v\n", err)
-	  return ""
-	}
-  
-	row := conn.QueryRow(context.Background(),
-	  "INSERT INTO phonebook (name, phone) VALUES ($1, $2) RETURNING id",
-	  rec.Name, rec.Phone)
-	var id uint64
-	err = row.Scan(&id)
-	if err != nil {
-		fmt.Errorf("Unable to INSERT: %v\n", err)
-
-	  return ""
-	}
-  
-	resp := make(map[string]string, 1)
-	resp["id"] = strconv.FormatUint(id, 10)
-	err = json.NewEncoder().Encode(resp)
-	if err != nil {
-		fmt.Errorf("Unable to encode json: %v\n", err)
-
-	  return ""
-	}
-	return "что-то"
-  }
